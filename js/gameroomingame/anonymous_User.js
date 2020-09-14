d4e38a577f9ce7e615e5e13f05a5efefcc8c30ed
tmehfld <tmehfld@naver.com>
2020-09-14 14:54:03 +0900

    123

---
+++ js/gameroomingame/anonymous_User.js
@@ -0,0 +1,16 @@
+/**
+ * anonymous user class
+ */
+class annymUser{
+	
+//	constructor(key,id,timestamp,priority) {
+//		this.key = key;
+//        this.id = id;
+//        this.timestamp =timestamp;
+//        this.priority = priority; 
+//    }
+    constructor(key,userdata) {
+		this.key = key;
+        this.userdata = userdata;
+    }
+}
