adbe806c0a554c5cf8bce13e2988c79f4e6c2e17
tmehfld <tmehfld@naver.com>
2020-09-22 22:33:08 +0900

    basic_function

---
+++ js/firebase_initiate/firebase_initiate.js
@@ -0,0 +1,16 @@
+/**
+ * anonymous user class
+ */
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCqVORIxXazIGcKlIaGTKn4YgX8R6Kg5Os",
+    authDomain: "fir-chattutorial-5f637.firebaseapp.com",
+    databaseURL: "https://fir-chattutorial-5f637.firebaseio.com",
+    projectId: "firebasechattutorial",
+    storageBucket: "firebasechattutorial.appspot.com",
+    
+    messagingSenderId: "1084417645508",
+    appId: "1:1084417645508:web:a86d313330cba20b0f2291"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
